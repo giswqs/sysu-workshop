8920f724d060888a4c5b7f3a42c6c4c6de7b10c6
Qiusheng Wu <Qiusheng Wu>
2017-07-20 16:44:01 +0800

    add notebook demo

--- R/projects/1_Intro.R
+++ R/projects/1_Intro.R
@@ -1,6 +1,11 @@
 1+1
+1:1000
 max(1,3,5)
 q()
+
+min(5:1, pi) #-> one number
+pmin(5:1, pi) #->  5  numbers
+
 help(max)
 ?max
 example(max)
@@ -12,6 +17,9 @@ sqrt(2)
 print(pi)
 print(matrix(c(1,2,3,4),2,2))
 
+X <- 44
+
+x <- 1
 # To insert the assignment operator <- use the following:
 #   
 #   Windows/Linux: "Alt" + "-"
@@ -24,6 +32,8 @@ z
 x <- "geography"
 print(x)
 
+
+
 ls()
 ls.str()
 
@@ -31,9 +41,13 @@ ls.str()
 rm(x)
 ls()
 
+
+
 c(1,3,5,7)
+c(1:100)
 c("a","b","c")
 c(TRUE,FALSE,TRUE)
+
 
 v1 <- c(1,3,5)
 v2 <- c(2,4,6)
@@ -44,16 +58,37 @@ mode(v1)
 mode("a")
 
 
-num <- 1:10
+num <- 1:10010
 num
 mean(num)
 median(num)
 sd(num)
 var(num)
 
+
 df <- cars
 str(df)
 summary(df)
 fix(df)
 
+
+df$speed
 plot(df$speed,df$dist)
+
+df$dist
+
+df[1,1]
+df[10,2]
+
+df[5,]
+df[,2]
+
+df[c(1,3,5,7),]
+
+df[seq(1,50,3),]
+
+seq(from = 1, to = 999,by = 2)
+seq(1,999,5)
+
+seq(1,1000,2)
+seq(1,1000,length.out = 300)
