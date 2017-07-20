067b64219cb7cdf405f0f1d3fd81dc937401255a
Qiusheng Wu <Qiusheng Wu>
2017-07-20 23:42:10 +0800

    add regression

---
+++ R/projects/4_correlation.R
@@ -0,0 +1,33 @@
+## Correlation
+
+x <- rnorm(20)
+y <- rnorm(20)
+
+cor(x,y)
+cor.test(x,y)
+
+cor.test(x,y,method="spearman")
+
+df <- read.csv("data/ExamAnxiety.csv",header = TRUE)
+plot(df)
+
+x <- seq(from=0,to = 1,length.out = 100)
+y <- x + rnorm(100)
+plot(x,y)
+
+df <- mtcars
+cor(df)
+plot(df)
+
+plot(df$hp,df$qsec)
+abline(lm(df$qsec ~ df$hp))
+
+mpg <- df$mpg
+wt <- df$wt
+plot(wt, mpg)
+
+examData <- read.csv("data/ExamAnxiety.csv",header = TRUE)
+cor(examData, use = "complete.obs", method = "pearson")
+cor(examData$Exam, examData$Anxiety, use = "complete.obs", method = "pearson")
+cor.test(examData$Exam, examData$Anxiety, method = "pearson")
+
